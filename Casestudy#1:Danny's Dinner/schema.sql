2dca6492b4111315a3da4f42cff6ca53d2db9e39
Megzu07 <109617407+Megzu07@users.noreply.github.com>
2023-10-17 22:37:26 +0530

    Update schema.sql

--- Casestudy#1:Danny's Dinner/schema.sql
+++ Casestudy#1:Danny's Dinner/schema.sql
@@ -1,1 +1,15 @@
+1.What is the total amount each customer spent at the restaurant?
+  
+SELECT s.customer_id, SUM(m.price)
+FROM dannys_diner.menu m
+INNER JOIN dannys_diner.sales s ON (m.product_id = s.product_id)
+GROUP BY s.customer_id
+ORDER BY s.customer_id;
 
+output: 
+  
+| customer_id | sum |
+|------------|-----|
+| A          | 76  |
+| B          | 74  |
+| C          | 36  |
